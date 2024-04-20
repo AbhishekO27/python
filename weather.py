8bea17063fd10ebca406bc36caa29aa3b31bd045
Abhishek Bora <158581886+AbhishekO27@users.noreply.github.com>
2024-04-20 13:35:38 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,8 +6,8 @@ user_input = input("enter city : ")
 
 weather_data = request.get(f"https://api.openweather.org/data/2.5/weather?q={user_input}&units=imperial&APIID={api_key}")
 
- weather = weather_data.json() ['weather'][0]['main']
- temp = round(weather_data.json()['main']['temp'])
+weather = weather_data.json() ['weather'][0]['main']
+temp = round(weather_data.json()['main']['temp'])
  
- print(f"weather in {user_input} is = {weather}  )
-  print(f"temperature in {user_input} is = {temp}F  )
+print(f"weather in {user_input} is = {weather} " )
+print(f"temperature in {user_input} is = {temp}F " )
